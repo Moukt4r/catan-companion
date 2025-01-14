550062a8ac9f99fd740d70f6dc794cfd902dd242
Moukt4r <65290303+Moukt4r@users.noreply.github.com>
2025-01-14 19:42:15 +0100

    refactor: add component exports

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from './Header';
+export { DiceRoller } from './DiceRoller';
+export { default as DiceDisplay } from './DiceDisplay';
+export { GameEvents } from './GameEvents';
+export { BarbarianTracker } from './BarbarianTracker';
+export { ErrorBoundary } from './ErrorBoundary';
