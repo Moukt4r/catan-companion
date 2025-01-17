efbc366f9b7e7921e7390a77ec5ef24597885bcf
Moukt4r <65290303+Moukt4r@users.noreply.github.com>
2025-01-17 20:09:14 +0100

    chore: add Jest setup file

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,69 @@
+// Add testing-library jest-dom matchers
 import '@testing-library/jest-dom';
+
+// Mock next/router
+jest.mock('next/router', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '',
+      query: '',
+      asPath: '',
+      push: jest.fn(),
+      replace: jest.fn(),
+    };
+  },
+}));
+
+// Mock next/dynamic for any dynamically imported components
+jest.mock('next/dynamic', () => ({
+  __esModule: true,
+  default: (...args) => {
+    const Component = args[0];
+    return Component;
+  },
+}));
+
+// Mock next/image
+jest.mock('next/image', () => ({
+  __esModule: true,
+  default: (props) => {
+    // eslint-disable-next-line jsx-a11y/alt-text, @next/next/no-img-element
+    return <img {...props} />;
+  },
+}));
+
+// Mock window.matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// Mock IntersectionObserver
+const mockIntersectionObserver = jest.fn();
+mockIntersectionObserver.mockReturnValue({
+  observe: () => null,
+  unobserve: () => null,
+  disconnect: () => null,
+});
+window.IntersectionObserver = mockIntersectionObserver;
+
+// Mock window.fs for file system operations
+window.fs = {
+  readFile: jest.fn(),
+};
+
+// Mock React 18 useId hook
+jest.mock('react', () => ({
+  ...jest.requireActual('react'),
+  useId: () => 'test-id',
+}));
