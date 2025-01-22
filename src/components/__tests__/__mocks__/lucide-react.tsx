f29d7d5c90b505804d4776ad6b6beb6d4dd037cc
Moukt4r <65290303+Moukt4r@users.noreply.github.com>
2025-01-22 12:19:03 +0100

    test: remove duplicate lucide-react mock file

--- src/components/__tests__/__mocks__/lucide-react.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-// Mock implementation for lucide-react icons
-export const RotateCcw = (props: any) => <div data-testid="rotate-ccw-icon" {...props} />;
-export const Loader = (props: any) => <div data-testid="loader-icon" {...props} />;
-export const Volume2 = (props: any) => <div data-testid="volume-2-icon" {...props} />;
-export const VolumeX = (props: any) => <div data-testid="volume-x-icon" {...props} />;
