7fc12bfa3e9e6297fe84647a9632567159641526
Moukt4r <65290303+Moukt4r@users.noreply.github.com>
2025-01-22 12:39:05 +0100

    Remove component-level mock file

--- src/components/__tests__/__mocks__/lucide-react.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-
-export const Volume2 = () => <span data-testid="volume-2-icon">Volume2</span>;
-export const VolumeX = () => <span data-testid="volume-x-icon">VolumeX</span>;
-export const Loader = () => <span data-testid="loader-icon">Loader</span>;
-export const RotateCcw = () => <span data-testid="rotate-ccw-icon">RotateCcw</span>;
