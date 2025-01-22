d5f68a39aa08a7647d54053f5a622a89440eb609
Moukt4r <65290303+Moukt4r@users.noreply.github.com>
2025-01-22 11:14:30 +0100

    test: add Lucide icons mock for testing

---
+++ src/components/__tests__/__mocks__/lucide-react.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export const Loader = ({...props}: {[key: string]: any}) => <span>Loading Icon</span>;
+export const RotateCcw = ({...props}: {[key: string]: any}) => <span>Reset Icon</span>;
+export const Volume2 = ({...props}: {[key: string]: any}) => <span>Sound On Icon</span>;
+export const VolumeX = ({...props}: {[key: string]: any}) => <span>Sound Off Icon</span>;
