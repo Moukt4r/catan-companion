e63631c8397584b052759eaab781fdda4d38e789
Moukt4r <65290303+Moukt4r@users.noreply.github.com>
2025-01-24 20:21:12 +0100

    Add Audio API mock to jest setup

--- jest.setup.js
+++ jest.setup.js
@@ -23,3 +23,15 @@ Object.defineProperty(window, 'localStorage', {
   },
   writable: true
 });
+
+// Mock Audio API
+class MockAudio {
+  play() {
+    return Promise.resolve();
+  }
+}
+
+Object.defineProperty(window, 'Audio', {
+  writable: true,
+  value: MockAudio
+});
